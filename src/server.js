ac1482d4a42e4427ce25c15d4d643be155ab266a
Agatha North <rouborn@gmail.com>
2020-03-24 13:03:56 -0700

    added basic structure

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const morgan = require('morgan');
+const data = require('./moviedata.json');
+const app = express();
+app.use(morgan('dev'));
+
+
+const API_TOKEN = 
+
+function accessMovie(req, res){
+
+}
+
+app.listen('3000', () => console.log('I hope this works'));
