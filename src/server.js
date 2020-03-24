1652a10faf6f89a572a8c67b198c3454e4285bd6
Jordan <jordanknox@Jordans-MacBook-Pro-3.local>
2020-03-24 16:03:14 -0700

    final

--- src/server.js
+++ src/server.js
@@ -8,23 +8,25 @@ const data = require('./moviedata.json');
 const app = express();
 
 app.use(morgan('dev'));
-app.use(helmet());
-app.use(cors());
-app.use(validateAuth);
+app.use(helmet()); // disables header by default. makes more secure
+app.use(cors()); // CORS-origin resource sharing. won't allow anyone touch a backend.
+app.use(validateAuth); // checks all validation for all authorization endpoints)
 
+
+// Validating Authorization before matching path, and moving to next step
 const API_TOKEN = process.env.API_TOKEN;
 
-function validateAuth(req, res, next){
-    const authValue = req.get('Authorization');
+function validateAuth(req, res, next){  // Validating response using middleware
+    const authValue = req.get('Authorization'); // Grabbing the Authorization property. 
     if (authValue === undefined){
-        return res.status(400).json({ error: 'Authorization header missing' })
+        return res.status(400).json({ error: 'Authorization header missing'})
     }
 
-    if (!authValue.toLowerCase().startsWith('bearer')){
+    if (!authValue.toLowerCase().startsWith('bearer')) { // bearer is a type of Authorization
         return res.status(400).json({ error: 'Invalid Authorization method: Must use Bearer strategy' })
     }
 
-    const token = authValue.split(' ')[1];
+    const token = authValue.split(' ')[1]; // putting token in second position 
     if (token !== API_TOKEN){
         return res.status(401).json({ error: 'Invalid credentials' })
     }
@@ -35,20 +37,24 @@ function validateAuth(req, res, next){
 function accessMovie(req, res){
     let movies = [...data];
     const search = ['genre','country','avg_vote'];
-
-    if (!search.includes(req.query)){
-        return res.status(400).send({ error: `oh no, you did it all wrong`})
+    
+    for (const key in req.query) { // Loop through through keys in query object
+        if (!search.includes(key)) { // if it does not include search method in array, throw this status
+            return res.status(400).send({
+                error: `oh no, you did it all wrong`
+            })
+        }
     }
 
     if (req.query.genre){
-        movies = movies.filter(movie => movie.genre.toLowerCase(req.query.genre))
+        movies = movies.filter(movie => movie.genre.toLowerCase() === req.query.genre.toLowerCase()) // comparing if movie genre search is lowercase with what we have on the server
     }
 
     if (req.query.country){
-        movies = movies.filter(movie => movie.country.toLowerCase(req.query.country))
+        movies = movies.filter(movie => movie.country.toLowerCase() === req.query.country.toLowerCase())
     }
 
-    if (req.query.avg_vote){
+    if (req.query.avg_vote){ // If true, filter movies that are equal to or higher than original query
         movies = movies.filter(movie => Number(movie.avg_vote) >= Number(req.query.avg_vote))
     }
 
